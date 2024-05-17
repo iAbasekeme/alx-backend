5a83d784c4a7aa47bbc049c5e44b7d434567af3e
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-05-17 18:44:07 +0100

    fixed doctest error

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-'''A module that calculates pagination
-'''
+"""A module that calculates pagination
+"""
 
 import csv
 import math
@@ -8,8 +8,8 @@ from typing import List
 
 
 def index_range(page, page_size):
-    '''A method that calculates pagination for an api
-    '''
+    """A method that calculates pagination for an api
+    """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
@@ -35,9 +35,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        '''A method that return a tuple of size two containing a start index and an
-        end index corresponding to the range of indexes to return in a list
-        '''
+        """A method that return a tuple of size two containing a start index and an
+           end index corresponding to the range of indexes to return in a list
+        """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
