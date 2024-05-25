44068f5416c3988d28d92d999a98072f738baeb3
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-05-25 13:16:20 +0100

    Update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,24 +5,24 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-  """ Basecaching class that defines a dictionary caching system
-  """
-
-  def __init__(self):
-    """ Initialization of the class
+    """ Basecaching class that defines a dictionary caching system
     """
-    super().__init__()
 
-  def put(self, key, item):
-    """ experimenting caching with dictionary
-    """
-    if key is None or item is None:
-      return
-    self.cache_data[key] = item
+    def __init__(self):
+        """ Initialization of the class
+        """
+        super().__init__()
 
-  def get(self, key):
-    """A method that gets a value by it's key
-    """
-    if key is None:
-      return None
-    return self.cache_data.get(key)
+    def put(self, key, item):
+        """ experimenting caching with dictionary
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """A method that gets a value by it's key
+        """
+        if key is None:
+            return None
+        return self.cache_data.get(key)
