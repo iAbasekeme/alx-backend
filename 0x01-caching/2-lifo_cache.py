a9b5dbcc14639edd86915c49e59d3364c8abe7c0
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-05-25 13:22:52 +0100

    Update 2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -14,15 +14,16 @@ class LIFOCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
-        """ Discarding the frist element of a cache
+        """Adds an item in the cache.
         """
         if key is None or item is None:
             return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
         self.cache_data[key] = item
-        if len(self.cache_data) > self.MAX_ITEMS:
-            last_key = list(self.cache_data.keys())[-1]
-        print("DISCARD:", last_key)
-        del self.cache_data[last_key]
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
         """ Get a value of a key in the cache
