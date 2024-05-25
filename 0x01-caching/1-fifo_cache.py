08749d976976e9b4247409d36b28847bfa53a934
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-05-25 13:28:13 +0100

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,34 +1,32 @@
-#!/usr/bin/python3
-""" FIFO caching
+"""First-In First-Out caching module.
 """
 from collections import OrderedDict
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """FIFO caching
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a FIFO
+    removal mechanism when the limit is reached.
     """
 
     def __init__(self):
-        """ Initialize
+        """Initializes the cache.
         """
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Discarding the frist element of a cache
+        """Adds an item in the cache.
         """
         if key is None or item is None:
             return
         self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key = next(iter(self.cache_data))
-        print("DISCARD: {}".format(first_key))
-        del self.cache_data[first_key]
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
 
     def get(self, key):
-        """ Get a value of a key in the cache
+        """Retrieves an item by key.
         """
-        if key not in self.cache_data | key is None:
-            return None
-        return self.cache_data[key]
+        return self.cache_data.get(key, None)
