96f187a6a449d61234643176a73d11fcfb08273b
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-05-25 13:20:07 +0100

    pycodestyle fix

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -5,28 +5,28 @@ from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-  """FIFO caching
-  """
-
-  def __init__(self):
-    """ Initialize
+    """FIFO caching
     """
-    super().__init__()
 
-  def put(self, key, item):
-    """ Discarding the frist element of a cache
-    """
-    if key is None or item is None:
-      return
-    self.cache_data[key] = item
-    if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-      first_key = next(iter(self.cache_data))
-      print("DISCARD: {}".format(first_key))
-      del self.cache_data[first_key]
+    def __init__(self):
+        """ Initialize
+        """
+        super().__init__()
 
-  def get(self, key):
-    """ Get a value of a key in the cache
-    """
-    if key not in self.cache_data | key is None:
-      return None
-    return self.cache_data[key]
+    def put(self, key, item):
+        """ Discarding the frist element of a cache
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key = next(iter(self.cache_data))
+        print("DISCARD: {}".format(first_key))
+        del self.cache_data[first_key]
+
+    def get(self, key):
+        """ Get a value of a key in the cache
+        """
+        if key not in self.cache_data | key is None:
+            return None
+        return self.cache_data[key]
--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -5,28 +5,28 @@ from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-  """ LIFO caching
-  """
-
-  def __init__(self):
-    """ Initialize
+    """ LIFO caching
     """
-    super().__init__()
 
-  def put(self, key, item):
-    """ Discarding the frist element of a cache
-    """
-    if key is None or item is None:
-      return
-    self.cache_data[key] = item
-    if len(self.cache_data) > self.MAX_ITEMS:
-      last_key = list(self.cache_data.keys())[-1]
-      print("DISCARD:", last_key)
-      del self.cache_data[last_key]
+    def __init__(self):
+        """ Initialize
+        """
+        super().__init__()
 
-  def get(self, key):
-    """ Get a value of a key in the cache
-    """
-    if key not in self.cache_data | key is None:
-      return None
-    return self.cache_data[key]
+    def put(self, key, item):
+        """ Discarding the frist element of a cache
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > self.MAX_ITEMS:
+            last_key = list(self.cache_data.keys())[-1]
+        print("DISCARD:", last_key)
+        del self.cache_data[last_key]
+
+    def get(self, key):
+        """ Get a value of a key in the cache
+        """
+        if key not in self.cache_data | key is None:
+            return None
+        return self.cache_data[key]
