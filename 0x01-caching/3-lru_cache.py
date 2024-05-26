587258b7b33b932d72ff5daea45de19a72860f54
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-05-26 01:47:15 +0100

    Update 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -32,6 +32,4 @@ class LRUCache(BaseCaching):
     def get(self, key):
         """Retrieves an item by key.
         """
-        if key is not None and key in self.cache_data:
-            self.cache_data.move_to_end(key, last=False)
         return self.cache_data.get(key, None)
