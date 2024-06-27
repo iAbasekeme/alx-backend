3a0b6917cabbf6ffe4b2ce0618aa82c531a6412e
Abasiekeme Etim <abasiekemeetim20@gmail.com>
2024-06-27 10:03:19 +0100

    Node Redis client and basic operations

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,11 +1,11 @@
-import { createClient } from "redis";
+import { createClient } from 'redis';
 
 const client = createClient();
 
-client.on("connect", () => {
-  console.log(`Redis client connected to the server`);
-});
-client.on("error", (err) => {
-  console.error(`Redis client not connected to the server: `, err);
+client.on('connect', function() {
+  console.log('Redis client connected to the server');
 });
 
+client.on('error', (err) => {
+  console.error('Redis client not connected to the server: ', err);
+});
